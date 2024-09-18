e261d61f235fb3d8105c61be0b18a884fc839a54
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-18 22:37:37 +0300

    updating the screen breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,13 @@ const config: Config = {
       },
       white: "#ffffff",
     },
+    screens: {
+      xs: "576px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 };
