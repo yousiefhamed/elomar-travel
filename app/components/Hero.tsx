38a658048d029d0456af58995212e34b833ea0f9
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-18 23:02:21 +0300

    fixing mobile layout & updating the heroIcon to be more responsive

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,11 +5,14 @@ const Hero = () => {
   return (
     <section className="w-full grid grid-cols-1 lg:grid-cols-2 xs:gap-10 gap-36 h-svh max-h-[650px] justify-center items-center">
       <div className="text-right flex flex-col gap-5 justify-center items-start">
-        <h1 className="text-6xl font-bold text-blue-deep leading-[140%]">
+        <h1 className="md:text-6xl text-5xl font-bold text-blue-deep leading-[140%]">
           رحلات مخصصة لك بأمان وراحة.
         </h1>
         <p className="text-3xl font-medium text-decor-gray-text leading-[140%] relative">
-          نحن هنا لجعل كل رحلة ذكرى
+          نحن هنا لجعل كل رحلة ذكرى{" "}
+          <span className="text-green-light font-bold xs:hidden inline-block">
+            جميلة
+          </span>
           <Image
             src="/icon-hero.svg"
             alt="جميلة"
@@ -20,7 +23,19 @@ const Hero = () => {
               left: 0,
               transition: "filter 0.5s ease-in-out",
             }}
-            className="xs:-translate-x-1/2 -translate-x-[40px] xs:-translate-y-1/4 xs:top-0 top-[45px] -z-20"
+            className="xs:hidden inline-block xs:-translate-x-1/2 translate-x-0 xs:-translate-y-1/4 xs:top-0 top-[45px] -z-20"
+          />
+          <Image
+            src="/icon-hero-text.svg"
+            alt="جميلة"
+            width={300}
+            height={300}
+            style={{
+              position: "absolute",
+              left: 0,
+              transition: "filter 0.5s ease-in-out",
+            }}
+            className="xs:inline-block hidden xs:-translate-x-1/2 -translate-x-[40px] xs:-translate-y-1/4 xs:top-0 top-[45px] -z-20"
           />
         </p>
         <Btn title="احجز الآن" customStyle="" />
