c847082c258aa22f8ceb9de75a7d77099a9c7344
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-19 10:26:45 +0300

    adding whatsapp btn as a util

---
+++ app/components/utils/WhatsappBtn.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Btn from "./Btn";
+
+const WhatsappBtn = () => {
+  return (
+    <div className="relative w-fit hover:scale-110 transition-transform duration-300">
+      <Image
+        src="/icons/whatsapp3D.png"
+        alt="whatsapp3D icon"
+        width={78}
+        height={97}
+        className="absolute top-1/2 right-0 translate-x-1/2 -translate-y-1/2 w-16 h-24 transition-transform duration-300 z-40"
+      />
+      <Btn title="استفسر الان" customStyle="pr-12 hover:scale-100" />
+    </div>
+  );
+};
+
+export default WhatsappBtn;
