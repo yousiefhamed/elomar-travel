5a0ccc7c318a2508a1160ed8fcac12cb46293733
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-20 00:08:38 +0300

    adding bg prop

--- app/components/utils/BgScreen.tsx
+++ app/components/utils/BgScreen.tsx
@@ -1,8 +1,12 @@
-const BgScreen = () => {
+const BgScreen: React.FC<{ bg?: string }> = ({
+  bg = "bg-decor-gray-light",
+}) => {
   return (
     <div
       style={{ width: "calc(100svw - 5px)" }}
-      className="bg-decor-gray-light h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-10 box-content -z-50"
+      className={`${
+        bg !== "bg-decor-gray-light" ? bg : "bg-decor-gray-light"
+      } h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-10 box-content -z-50`}
     ></div>
   );
 };
