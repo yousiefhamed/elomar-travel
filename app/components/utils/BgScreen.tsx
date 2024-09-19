8b9dae745a0662415621e26a602272564932f4f0
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-19 10:56:43 +0300

    adding bg full screen utils

---
+++ app/components/utils/BgScreen.tsx
@@ -0,0 +1,10 @@
+const BgScreen = () => {
+  return (
+    <div
+      style={{ width: "calc(100svw - 5px)" }}
+      className="bg-decor-gray-light h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-10 box-content"
+    ></div>
+  );
+};
+
+export default BgScreen;
