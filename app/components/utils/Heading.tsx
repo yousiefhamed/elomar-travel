27a0b52d2968452e63b99c0d619bb6a02b9af7c6
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-19 10:14:24 +0300

    fixing color from dark to light

--- app/components/utils/Heading.tsx
+++ app/components/utils/Heading.tsx
@@ -1,7 +1,7 @@
 const Heading: React.FC<{ title: string }> = ({ title }) => {
   return (
     <div className="w-full pb-12">
-      <h2 className="w-fit text-5xl font-bold relative after:absolute after:top-full after:right-0 after:w-3/4 after:bg-green-dark after:h-[6px] after:mt-2 after:rounded-l-full">
+      <h2 className="w-fit text-5xl font-bold relative after:absolute after:top-full after:right-0 after:w-3/4 after:bg-green-light after:h-[6px] after:mt-2 after:rounded-l-full">
         {title}
       </h2>
     </div>
