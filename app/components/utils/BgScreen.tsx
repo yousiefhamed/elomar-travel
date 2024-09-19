56ca1be77a21aa12660bb1455218ac6a396194da
Youssef Hamed <yousiefhamed@gmail.com>
2024-09-19 13:26:06 +0300

    adding -z-index to BgScreen so it always be behind and increase p-y of Btn

--- app/components/utils/BgScreen.tsx
+++ app/components/utils/BgScreen.tsx
@@ -2,7 +2,7 @@ const BgScreen = () => {
   return (
     <div
       style={{ width: "calc(100svw - 5px)" }}
-      className="bg-decor-gray-light h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-10 box-content"
+      className="bg-decor-gray-light h-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-10 box-content -z-50"
     ></div>
   );
 };
--- app/components/utils/Btn.tsx
+++ app/components/utils/Btn.tsx
@@ -15,7 +15,7 @@ const Btn = ({
       href="https://wa.me/+201208774786?text=مرحبًا، أنا مهتم بحجز رحلة معكم من خلال الموقع الإلكتروني."
       target="_blank"
       rel="noopener noreferrer"
-      className={`w-fit flex justify-center items-center gap-3 rounded-xl pr-8 pl-5 py-2 text-xl font-bold text-white bg-green-dark shadow-btn hover:scale-110 transition-transform duration-300 ${customStyle}`}
+      className={`w-fit flex justify-center items-center gap-3 rounded-xl pr-8 pl-5 py-3 text-xl font-bold text-white bg-green-dark shadow-btn hover:scale-110 transition-transform duration-300 ${customStyle}`}
     >
       {title}
       <Icon icon="solar:arrow-left-broken" className="w-7 h-7 font-bold" />
